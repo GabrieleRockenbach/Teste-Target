95dd62e1cef176c3d370a15e6281ac51a12994b7
Gabriele <gabrielerockenbach@outlook.com>
2024-02-25 23:28:24 -0300

    :ok_hand: style: alteração dos diretórios do projeto.

--- main.js
+++ main.js
@@ -5,25 +5,25 @@ const cardContent = [
         title: 'Teste 1: Sequência Fibonacci',
         image: './assets/teste-1.jpg',
         leftButtonAnchor: '/Teste_1/index.html',
-        rightButtonAnchor: '#'
+        rightButtonAnchor: 'https://github.com/GabrieleRockenbach/teste-target/blob/main/Teste_1/fibonacci.js'
     },
     {
         title: 'Teste 2: Dados de Faturamento',
         image: './assets/teste-2.jpg',
         leftButtonAnchor: '/Teste_2/index.html',
-        rightButtonAnchor: '#'
+        rightButtonAnchor: 'https://github.com/GabrieleRockenbach/teste-target/blob/main/Teste_2/media-faturamento.js'
     },
     {
         title: 'Teste 3: Porcentagem por Estado',
         image: './assets/teste-3.jpg',
         leftButtonAnchor: '/Teste_3/index.html',
-        rightButtonAnchor: '#'
+        rightButtonAnchor: 'https://github.com/GabrieleRockenbach/teste-target/blob/main/Teste_3/faturamento-estado.js'
     },
     {
         title: 'Teste 4: Palavra Reversa',
         image: './assets/teste-4.jpg',
         leftButtonAnchor: '/Teste_4/index.html',
-        rightButtonAnchor: '#'
+        rightButtonAnchor: 'https://github.com/GabrieleRockenbach/teste-target/blob/main/Teste_4/reverse-function.js'
     }
 ]
 
@@ -44,10 +44,13 @@ function showCards(){
         const cardLeftButton = document.createElement('a');
         cardLeftButton.textContent = 'Testar';
         cardLeftButton.setAttribute('href' , element.leftButtonAnchor);
+        cardLeftButton.setAttribute('target' , '_blank')
 
         const cardRightButton = document.createElement('a');
         cardRightButton.textContent = 'Ver Código';
         cardRightButton.setAttribute('href' , element.rightButtonAnchor);
+        cardRightButton.setAttribute('target' , '_blank')
+
 
         buttonContainer.appendChild(cardLeftButton);
         buttonContainer.appendChild(cardRightButton);
